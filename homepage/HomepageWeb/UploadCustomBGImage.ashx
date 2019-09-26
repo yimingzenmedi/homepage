e2c9e206728fdd776d81903afef14f1ca8759547
yimingzenmedi <31919394+yimingzenmedi@users.noreply.github.com>
2019-09-26 13:35:19 +1200

    v2.1.0

---
+++ homepage/HomepageWeb/UploadCustomBGImage.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="UploadCustomBGImage.ashx.cs" Class="HomepageWeb.UploadCustomBGImage" %>
---
+++ homepage/HomepageWeb/UploadCustomBGImage.ashx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using HomepageWeb.BLL;
+
+namespace HomepageWeb
+{
+    /// <summary>
+    /// UploadCustomBGImage 的摘要说明
+    /// </summary>
+    public class UploadCustomBGImage : IHttpHandler, IRequiresSessionState
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <return>
+        /// -1: invalid params
+        /// 0: ok
+        /// 1: nothing changed
+        /// 2: not login
+        /// </return>
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+
+            //check login state:
+            if (!new LoginStateManager().CheckLoginState(context))
+            {
+                context.Response.Write("2");
+                context.Response.End();
+            }
+
+            string siteName = context.Request.Form["siteName"];
+            string iconBase64 = context.Request.Form["iconBase64"];
+            string userName = context.Session["loginAs"].ToString();
+
+            if (siteName == null || siteName.Trim() == string.Empty)
+            {
+                context.Response.Write("-1");
+                context.Response.End();
+            }
+            if (iconBase64 == null || iconBase64.Trim() == string.Empty)
+            {
+                context.Response.Write("-1");
+                context.Response.End();
+            }
+
+            UserSitesManager userSitesManager = new UserSitesManager();
+            int result = userSitesManager.SetCustomBGImage(iconBase64, siteName, userName);
+            if (result == 0)
+            {
+                context.Response.Write("1");
+            }
+            else
+            {
+                context.Response.Write("0");
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
